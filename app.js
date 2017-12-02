a9a5f7bce83b1b2c33b9487a836432ea7be7eed7
Liz Bachman <eleighbachman@gmail.com>
2017-12-02 00:07:22 -0700

    playing around with accessing description and ids

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var $totalSelected = 0;
 var gameCardArray = [
   {
     name: "Mario Quairt",
@@ -22,12 +23,11 @@ var gameCardArray = [
 function createGameCards() {
   for (let i = 0; i < gameCardArray.length; i++) {
     var $gameName = gameCardArray[i].name;
-    console.log($gameName)
     var $gamePrice = gameCardArray[i].price;
     var $gameImg = gameCardArray[i].image;
     var $gameDesc = gameCardArray[i].description;
     var $newGame = $(
-   '<div class="card"><h2> ' + $gameName +
+   '<div class="card" id="'+ $gameName +'"><h2> ' + $gameName +
    '</h2><img src=' + $gameImg +
    '></div>'
     );
@@ -44,16 +44,35 @@ createGameCards();
 $('h1').css('display','none').fadeIn(2000);
 
 $('.card').on('click', function() {
-  console.log("clicked!")
+  if ($totalSelected < 3) {
+    $totalSelected += 1;
+    console.log($totalSelected);
+  }
   $(this).toggleClass('selected');
-  console.log($(this).id);
-  $('#descriptionBox').fadeIn(1600);
+  $('#descriptionBox').css('display','block');
+  $('#contentBox').empty();
+
+
+
 });
 
 $('#allSel').on('click', function() {
   $('.card').attr('class','card selected');
+  $totalSelected = 3;
+  $('#contentBox').empty();
+  $('#contentBox').append($('<br/>'));
+  $('#contentBox').append($('<p>You have selected all three games to buy.</p>'));
+  $('#descriptionBox').fadeIn(1600);
+  console.log($totalSelected);
 });
 
 $('#allDes').on('click', function() {
   $('.card').attr('class', 'card');
+  $totalSelected = 0;
+  $('#descriptionBox').css('display','none');
+  $('#contentBox').empty();
+  $('#contentBox').append($('<br/>'));
+  $('#contentBox').append($('<p>There are no games selected to buy.</p>'));
+  $('#descriptionBox').fadeIn(1600);
+  console.log($totalSelected);
 })
